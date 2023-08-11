8aa9d3dfd1a1699ef363693e7c6ec13a61066e13
Irvin Raul Lopez <irvin.lopez@autofact.com.mx>
2023-08-11 13:56:36 -0600

    update import command

--- app/Console/Commands/importGoogleCalendar.php
+++ app/Console/Commands/importGoogleCalendar.php
@@ -2,8 +2,8 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
 use App\Models\Reservation;
+use Illuminate\Console\Command;
 
 class importGoogleCalendar extends Command
 {
@@ -32,20 +32,38 @@ class importGoogleCalendar extends Command
         if ($archivo) {
             while (($linea = fgets($archivo)) !== false) {
                 if (strpos($linea, 'DTSTART') !== false) {
-                     // date format from text: 20230401
-                    $reservationDate = str_replace('DTSTART;VALUE=DATE:', '', $linea);
-                    $reservationDate = substr($reservationDate, 0, 4) . '-' . substr($reservationDate, 4, 2) . '-' . substr($reservationDate, 6, 2);
-                    $reservationDate = date('Y-m-d H:i:s', strtotime($reservationDate . ' +12 hours'));
+                    $this->info($linea);
+                    // date format from text: 20230401
+                    if (str_contains($linea, 'DTSTART;VALUE=DATE:') !== false) {
+                        $reservationDate = str_replace('DTSTART;VALUE=DATE:', '', $linea);
 
-                    $this->info($reservationDate);
+                    } else {
+                        // date format from text: 20230806T010000Z
+                        $reservationDate = str_replace('DTSTART:', '', $linea);
+                        $reservationDate = explode('T', $reservationDate)[0];
+                    }
+
+                    $reservationDate = substr($reservationDate, 0, 4).'-'.substr($reservationDate, 4, 2).'-'.substr($reservationDate, 6, 2);
+                    $reservationDate = date('Y-m-d H:i:s', strtotime($reservationDate.' +12 hours'));
+
                     $description = '';
                     while (($linea = fgets($archivo)) !== false) {
                         if (strpos($linea, 'DESCRIPTION') !== false) {
                             $description .= $linea;
                             $linea = fgets($archivo);
+                            if (strpos($linea, 'END:VEVENT') !== false) {
+                                break;
+                            }
                             if (str_contains($linea, 'LAST-MODIFIED') !== true) {
                                 $description .= $linea;
                             }
+                            break;
+                        }
+                        if (strpos($linea, 'SUMMARY') !== false) {
+                            $description .= $linea;
+                            break;
+                        }
+                        if (strpos($linea, 'END:VEVENT') !== false) {
                             break;
                         }
                     }
@@ -54,21 +72,24 @@ class importGoogleCalendar extends Command
                             $description .= $linea;
                             break;
                         }
+                        if (strpos($linea, 'END:VEVENT') !== false) {
+                            break;
+                        }
                     }
+
                     $notes = str_replace('\n', ' / ', str_replace('SUMMARY:', '', str_replace('DESCRIPTION:', '', $description)));
-                    $this->info($notes);
 
                     $reservation = Reservation::where('reservation_date', $reservationDate)->first();
 
-                    if (!$reservation) {
+                    if (! $reservation) {
                         $reservation = new Reservation();
                         $reservation->reservation_date = $reservationDate;
-                        $reservation->notes = $notes;
+                        $reservation->notes = $notes ?? null;
                         $reservation->is_approved = 1;
                         $reservation->save();
-                        $this->info('Reservation created.');
+                        $this->info('Reservation created: '.$reservationDate);
                     } else {
-                        $this->error('Reservation already exists.');
+                        $this->error('Reservation already exists: '.$reservationDate);
                     }
                 }
             }
